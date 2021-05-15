fb0f764949f95b5fd238498a76759ea3334d939c
cartereric <eric.eugene.carter@gmail.com>
2021-05-15 19:17:37 -0400

    Create Footer Microfront End

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+    let bananaSrc = "http://localhost:5006/assets/images/bananas.png"
+</script>
+
+<style>
+  #banana-png {
+      width: 200px;
+  }
+</style>
+
+<div>
+    <a href="https://billyargel.com/" target="_blank" rel="noopener noreferrer">
+        <img id="banana-png" src={ bananaSrc } alt=""/>
+    </a>
+</div>
+<div>
+    <p class="float-end"><a href="#">Back to top</a></p>
+    <p>
+        Made by Eric Carter &middot; <a href="#">Privacy</a> &middot;
+        <a href="#">Terms</a>
+    </p>
+</div>
