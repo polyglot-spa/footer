5205ef50bae18ff6257993d4a69f3c61ad313f1d
cartereric <eric.eugene.carter@gmail.com>
2021-05-29 10:42:14 -0400

    Remove hr divider

--- src/App.svelte
+++ src/App.svelte
@@ -6,17 +6,27 @@
   #banana-png {
       width: 200px;
   }
+  #footer-text {
+      font-family: Bananas-Personal;
+      font-size: 50px;
+  }
+  #footer-link {
+      margin-top: 10px;
+  }
 </style>
 
-<div>
-    <a href="https://billyargel.com/" target="_blank" rel="noopener noreferrer">
-        <img id="banana-png" src={ bananaSrc } alt=""/>
-    </a>
-</div>
-<div>
-    <p class="float-end"><a href="#">Back to top</a></p>
-    <p>
-        Made by Eric Carter &middot; <a href="#">Privacy</a> &middot;
-        <a href="#">Terms</a>
-    </p>
+<div class="container-fluid pt-3">
+    <div>
+        <span id="footer-text" class="me-3">Do you like this font? Check out this artist!</span>
+        <a id="footer-link"  href="https://billyargel.com/" target="_blank" rel="noopener noreferrer">
+            <img id="banana-png" src={ bananaSrc } alt=""/>
+        </a>
+    </div>
+    <div class="mt-3">
+        <p class="float-end"><a href="#">Back to top</a></p>
+        <p>
+            Made by Eric Carter &middot; <a href="#">Privacy</a> &middot;
+            <a href="#">Terms</a>
+        </p>
+    </div>
 </div>
